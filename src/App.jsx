19ae46a90833e1be2019a2e4ac1e3e2879654a8c
Lennart Grosskreutz <lennart.grosskreutz@student.kit.edu>
2023-02-11 21:08:13 +0100

    Create game over stub

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ export default function App() {
   //lazy state initialisation
   const [playerData, setPlayerData] = useState(() => initPlayerData())
   //how many categories are unused? The game is over iff the value is 0
-  const numberCategoriesPerPlayer = playerData.pointsAccount.length
+  const numberCategoriesPerPlayer = playerData[0].pointsAccount.length
   const [unusedCategories, setUnusedCategories] = useState(numberPlayers * numberCategoriesPerPlayer)
 
   const [dice, setDice] = useState(allNewDice())
@@ -110,20 +110,16 @@ export default function App() {
       setRollsLeft(numberRollsPerTurn)
       setDice(allNewDice())
       setUnusedCategories(prev => prev - 1)
-      if (unusedCategories === 0) {
-        console.log("GAME OVER")
-      }
     }
     return updateSuccessful
   }
 
-  return (
-    <main>
-      <h1 className="title">Kniffel</h1>
-      <p className="instructions">Per turn, each player may roll at most three times.
-        After each roll, they may fix each die of their choice.
-        At the end of their turn, the active player must
-        choose a category and receives points according to the current dice values.</p>
+  let gameOver = unusedCategories === 0
+  // middle part contains dice info if game is going on, otherwise endscreen
+  let middlePart
+  if (!gameOver) {
+    middlePart = (
+      <div className="middle-part">
       <div className="dice-container">
         {diceElements}
         <div className="sorting-symbol">
@@ -136,6 +132,24 @@ export default function App() {
         Roll dice
       </button>
       <p>Rolls left: {rollsLeft}</p>
+      </div>
+    )
+  } else {
+    middlePart = (
+      <div className="middle-part">
+        GAME OVER
+      </div>
+    )
+  }
+
+  return (
+    <main>
+      <h1 className="title">Kniffel</h1>
+      <p className="instructions">Per turn, each player may roll at most three times.
+        After each roll, they may fix each die of their choice.
+        At the end of their turn, the active player must
+        choose a category and receives points according to the current dice values.</p>
+      {middlePart}
       <PlayerDataDisplay playerData={playerData} onCategoryClick={setCategoryToUsed} activePlayerId={activePlayerId} rollsLeft={rollsLeft} />
     </main>
   )
